1f79919de0f70753a941ed6a30033ceeba5df1a0
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-05-23 00:43:58 +0530

    some mild changes

--- Node_JS/app.js
+++ Node_JS/app.js
@@ -1,24 +1,10 @@
-const { readFile, writeFile } =require('fs');
+const { readFileSync , writeFileSync } =require('fs');
 
-readFile('./content/first.txt',(err,result)=>{
-    if(err){
-        console.log(err);
-        return;
-    }
-    const first = result;
-    readFile('./content/second.txt',(err,result)=>{
-        if(err){
-            console.log(err);
-            return;
-        }
-        const second = result;
-        writeFile('./content/result-async.txt',"Here is the result:"+first+second,(err,result)=>{
-            if(err){
-                console.log(err);
-                return;
-            }
-            console.log(result);
-        });
+const first =readFileSync('./content/first.','utf8');
+const second =readFileSync('./content/second.','utf8');
 
-    })
-})
+console.log(first,second);
+writeFileSync('./content/result-sync.txt','The result is:'+first+second);
+
+console.log('done');
+console.log('starting with next task');
