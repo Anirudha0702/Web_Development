92d91b22ef600ed3c60dd3a7bd951fbb360f0ebd
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-05-30 14:14:17 +0530

    Some mild changes

---
+++ PHP/14th.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>14th PHP Program</title>
+    </head>
+    <body>
+
+        <?php
+            function cube($num){
+                return $num * $num* $num;
+            }
+
+            echo cube(4);
+        ?>
+    </body>
+</html>
