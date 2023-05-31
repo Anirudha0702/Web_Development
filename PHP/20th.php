26d9ac26402083efa258d6a6029061599d84df48
rajdeep298 <rajdeepkundu2983@gmail.com>
2023-05-31 22:46:01 +0530

    Some mild changes

---
+++ PHP/20th.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            class Book{
+                var $title;
+                var $author;
+                var $pages;
+            }
+        ?>
+    </body>
+</html>
